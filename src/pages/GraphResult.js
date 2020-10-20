9564341673a829babbd07c7bf1c6a03207562f22
wjone <engel0088@gmail.com>
2020-10-20 15:41:17 +0900

    feat: calendar setup

--- src/pages/GraphResult.js
+++ src/pages/GraphResult.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { setCurrentDate } from '../actions/dateActions'
+import Calendar from 'react-calendar'
 
+import 'react-calendar/dist/Calendar.css'
 import imgRightArrow from '../assets/imgs/lt.png'
 import imgGood from '../assets/imgs/good.png'
 import imgBad from '../assets/imgs/bad.png'
@@ -11,6 +13,8 @@ import imgRA from '../assets/imgs/ra.png'
 
 const DISPLAY_WEEKLY = 1;
 const DISPLAY_DAILY = 2;
+const MONTHLY_CALENDAR = 3;
+const DAILY_CALENDAR = 4;
 
 const mapStateToProps = (state) => ({
   weekStart: state.date.weekStart,
@@ -24,6 +28,8 @@ const mapDispatchToProps = (dispatch) => ({
 
 const GraphResult = ({currentDate, weekStart, weekEnd, setDate}) => {
   const [displayMode, setDisplayMode] = useState(DISPLAY_WEEKLY)
+  const [showCalendar, setShowCalendar] = useState(false)
+  const [calendarMode, setCalendarMode] = useState('month')
 
   const onWeeklyMode = () => {
     setDisplayMode(DISPLAY_WEEKLY)
@@ -35,32 +41,68 @@ const GraphResult = ({currentDate, weekStart, weekEnd, setDate}) => {
 
   const onOpenCalendar = (calendarMode) => {
     console.log("calendarMode", calendarMode);
+    if(calendarMode === MONTHLY_CALENDAR) {
+      setCalendarMode('year')
+    } else {
+      setCalendarMode('month')
+    }
+
+    setShowCalendar(true)
   }
 
+  const onDateChanged = (v, e) => {
+    console.log("dateChanged", v)
+    setShowCalendar(false)
+    setDate(v)
+  }
+
+  const onMonthChanged = (v, e) => {
+    console.log("monthChanged", v)
+    setShowCalendar(false)
+    setDate(v)
+  }
   /**
    * Navigation Event Handlers
    */
   const onPrevMonth = () => {
     console.log("move to previous month");
+    let dateTemp = new Date(currentDate)
+    dateTemp.setMonth(dateTemp.getMonth() - 1)
+    setDate(dateTemp)
   }
 
   const onNextMonth = () => {
     console.log("move to next month");
+    let dateTemp = new Date(currentDate)
+    dateTemp.setMonth(dateTemp.getMonth() + 1)
+    setDate(dateTemp)
   }
 
   const onPrevDayOrWeek = () => {
     if (displayMode === DISPLAY_WEEKLY) {
       console.log("move to previous week");
+      let dateTemp = new Date(currentDate)
+      dateTemp.setDate(dateTemp.getDate() - 7)
+      setDate(dateTemp)
     } else {
       console.log("move to previous day");
+      let dateTemp = new Date(currentDate)
+      dateTemp.setDate(dateTemp.getDate() - 1)
+      setDate(dateTemp)
     }
   }
 
   const onNextDayOrWeek = () => {
-    if (displayMode === DISPLAY_DAILY) {
+    if (displayMode === DISPLAY_WEEKLY) {
       console.log("move to next week");
+      let dateTemp = new Date(currentDate)
+      dateTemp.setDate(dateTemp.getDate() + 7)
+      setDate(dateTemp)
     } else {
       console.log("move to next day");
+      let dateTemp = new Date(currentDate)
+      dateTemp.setDate(dateTemp.getDate() + 1)
+      setDate(dateTemp)
     }
   }
 
@@ -108,7 +150,7 @@ const GraphResult = ({currentDate, weekStart, weekEnd, setDate}) => {
       <div className="query-wrapper d-flex align-items-stretch">
         <div className="selector month-selector flex-grow-1 border-right d-flex justify-content-center align-items-center">
           <button className="navigate" onClick={onPrevMonth}><img src={imgLA} alt="prev-month" /></button>
-          <button className="navigate" onClick={() => onOpenCalendar(DISPLAY_WEEKLY)}>
+          <button className="navigate" onClick={() => onOpenCalendar(MONTHLY_CALENDAR)}>
             {currentDate.toLocaleString('default', { month: 'long'})}&nbsp;
             {currentDate.getFullYear()}
           </button>
@@ -116,7 +158,7 @@ const GraphResult = ({currentDate, weekStart, weekEnd, setDate}) => {
         </div>
         <div className="selector week-day-selector flex-grow-1 d-flex justify-content-center align-items-center">
           <button className="navigate" onClick={onPrevDayOrWeek}><img src={imgLA} alt="prev-year" /></button>
-          <button className="navigate" onClick={() => onOpenCalendar(DISPLAY_DAILY)}>
+          <button className="navigate" onClick={() => onOpenCalendar(DAILY_CALENDAR)}>
             { selectedDateOrWeek() }
           </button>
           <button className="navigate" onClick={onNextDayOrWeek}><img src={imgRA} alt="next-year" /></button>
@@ -124,6 +166,18 @@ const GraphResult = ({currentDate, weekStart, weekEnd, setDate}) => {
         <div className="selector mode-selector flex-grow-1 border-left d-flex justify-content-center">
           <button className={`weekly-mode ${displayMode === DISPLAY_WEEKLY && 'active'}`} onClick={onWeeklyMode}>7 days</button>
           <button className={`daily-mode ${displayMode === DISPLAY_DAILY && 'active'}`} onClick={onDailyMode}>24 hours</button>
+        </div>
+      </div>
+
+      <div className={`modal ${!showCalendar && 'd-none'}`}>
+        <div className="modal__content d-flex flex-column align-items-center justify-content-center">
+          <Calendar
+            className="calendar"
+            onChange={onDateChanged}
+            onClickMonth={onMonthChanged}
+            value={currentDate}
+            view={calendarMode}
+          />
         </div>
       </div>
     </div>
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,11 @@
 export function getFirstDayOfWeek(cDate) {
   let cDateTemp = new Date(cDate)
   let cd = cDateTemp.getDate() - cDateTemp.getDay()
-  return new Date(cDateTemp.setDate(cd))
+  return new Date(cDateTemp.setDate(cd + 1))
 }
 
 export function getEndDayOfWeek(cDate) {
   let cDateTemp = new Date(cDate)
   let cd = cDateTemp.getDate() - cDateTemp.getDay()
-  return new Date(cDateTemp.setDate(cd + 6))
+  return new Date(cDateTemp.setDate(cd + 7))
 }
